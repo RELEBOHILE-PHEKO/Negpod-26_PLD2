ffd41cf6aa0a1413ea586bf696b9541b0ef4ca7a
pmugabo <p.mugabo@alustudent.com>
2024-03-26 05:32:41 -0700

    IMPROVE AFRICA

---
+++ main.py
@@ -0,0 +1,7 @@
+class RawMaterial:
+   def _init_(self, name, region, town, quantity, crop_type):
+     self.name = name
+     self.region = region
+     self.town = town
+     self.quantity = quantity
+     self.crop_type = crop_type
